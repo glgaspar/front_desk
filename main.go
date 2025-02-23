3bbbd2c24f4770a6b0190597d7490d097f1281b3
Gustavo Gaspar <glgaspar@live.com>
2025-02-23 14:59:12 -0300

    apps and config routes setup

--- main.go
+++ main.go
@@ -18,6 +18,34 @@ func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Co
 	return t.templates.ExecuteTemplate(w, name, data)
 }
 
+type App struct {
+	Img  string
+	Name string
+	Path string
+}
+
+type Config struct {
+	Apps   []App `json:"Apps"`
+	Config []App `json:"Config"`
+}
+
+func (c *Config) Generate() error {
+	file, err := os.Open("routes.json")
+	if err != nil {
+		return err
+	}
+
+	bytesBuffer, err := io.ReadAll(file)
+	if err != nil {
+		return err
+	}
+
+	if err = json.Unmarshal(bytesBuffer, &c); err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func newTemplate() *Templates {
 	return &Templates{
@@ -29,7 +57,8 @@ func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 
-	var data any
+	var data = Config{}
+	data.Generate()
 
 	e.Static("/dist", "dist")
 	e.Renderer = newTemplate()
