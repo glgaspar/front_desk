60ab9a087e0301a0e98a3cf2fb000ab7162c619f
Gustavo Gaspar <glgaspar@live.com>
2025-02-23 22:08:54 -0300

    error page

--- controller/controller.go
+++ controller/controller.go
@@ -1,22 +1,24 @@
 package controller
 
 import (
-	"front_desk/models"
 	"front_desk/data"
+	"front_desk/models"
+
 	"github.com/labstack/echo/v4"
 )
 
-
 func Root(c echo.Context) error {
 	var data = models.RootConfig{}
-	data.Generate()
-	return c.Render(200, "index", data)
+	if err := data.Generate(); err != nil {
+		return c.Render(500, "error", err)
+	}
+	return c.Render(200, "root", data)
 }
 
 func PayChecker(c echo.Context) error {
-	data,err := data.GetPayChecker()
+	data, err := data.GetPayChecker()
 	if err != nil {
-		return c.Render(500, "error", err.Error())
+		return c.Render(500, "error", err)
 	}
 	return c.Render(200, "paychecker", data)
 }
