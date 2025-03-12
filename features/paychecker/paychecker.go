a8d0f14897b2fa518e3f34237b236130015a153d
Gustavo Gaspar <glgaspar@live.com>
2025-03-12 00:39:43 -0300

    nil on html is ugly

--- features/paychecker/paychecker.go
+++ features/paychecker/paychecker.go
@@ -11,8 +11,8 @@ type Bill struct {
 	Description string     `json:"description" db:"description"` // What you are paying
 	ExpDay      int        `json:"expDay" db:"expDay"`           // Expiration day
 	Path        string     `json:"path" db:"path"`               // Where to find the files
-	LastDate    *time.Time `json:"lastDate" db:"lastDate"`       // Date of last payment
-	Track       *bool      `json:"track" db:"track"`             // Is that bill active?
+	LastDate    time.Time `json:"lastDate" db:"lastDate"`       // Date of last payment
+	Track       bool      `json:"track" db:"track"`             // Is that bill active?
 }
 
 func (b *Bill) GetAllBills() ([]Bill, error) {
