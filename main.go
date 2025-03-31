1b8e2512df80d5ad90f408e94e6e54e75c7938a2
Gustavo Gaspar <glgaspar@live.com>
2025-03-30 23:32:35 -0300

    user login validation + redirect

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"io"
 	"log"
+	"os"
 	"text/template"
 
 	"github.com/glgaspar/front_desk/router"
@@ -31,21 +32,54 @@ func NewTemplates() *Template {
 	}
 }
 
+
+func redirect(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		var bypass bool = false
+		if c.Path() == "/signup" { bypass = true }
+		if bypass { return next(c) }
+		if os.Getenv("FIRST_ACCESS") == "YES" {
+			return c.Redirect(301, "/signup")
+		}
+
+		if c.Path() == "/login" { bypass = true }
+		if bypass { return next(c) }
+
+		cookie, err := c.Cookie("front_desk_awesome_cookie") 
+		if (err != nil || cookie == nil) {
+			return c.Redirect(301, "/login")
+		}
+
+		valid, err := controller.LoginValidator(cookie)
+		if (err != nil || !valid) {
+			return c.Redirect(301, "/login")
+		}
+		
+		return next(c)
+	}
+}
+
+
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Printf("error reading .env %s", err.Error())
 	}
 
-	controller.CheckForUsers() // just setting stuff up
+	if err := controller.CheckForUsers(); err != nil { // just setting stuff up
+		panic(err)
+	} 
 
 	e := echo.New()
     e.Renderer = NewTemplates()
     e.Use(middleware.Logger())
+	e.Use(redirect)
 
 	e.GET("/", router.Root)
-	// e.GET("/login", router.Login)
-	// e.POST("/login", controller.Login)
+	e.GET("/login", router.Login)
+	e.POST("/login", controller.Login)
+	e.GET("/signup", router.Signup)
+	e.POST("/signup", controller.Signup)
 	e.GET("/paychecker", router.ShowPayChecker)
 	e.PUT("/paychecker/flipTrack/:billId", controller.FlipPayChecker)
 	e.POST("/paychecker/new", controller.NewPayChecker)
