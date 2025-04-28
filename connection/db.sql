402c018bf84cdaceb4bed5ff1b28398046d6be93
Gustavo Gaspar <glgaspar@live.com>
2025-04-27 21:23:13 -0300

    db stuff

--- connection/db.sql
+++ connection/db.sql
@@ -1,9 +1,22 @@
 create database frontdesk;
 
--- DROP SCHEMA paychecker;
+CREATE SCHEMA adm;
+CREATE TABLE adm.users (
+	id serial4 NOT NULL,
+	username varchar(50) NOT NULL,
+	"password" varchar(255) NOT NULL,
+	CONSTRAINT users_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE adm.activesessions (
+	userid int4 NOT NULL,
+	"token" varchar(255) NOT NULL,
+	CONSTRAINT activesessions_pkey PRIMARY KEY (userid)
+);
+ALTER TABLE adm.activesessions ADD CONSTRAINT activesessions_userid_fkey FOREIGN KEY (userid) REFERENCES adm.users(id);
+
 
 CREATE SCHEMA paychecker;
-
 create table paychecker.bills (
     id serial primary key, --id is just id
     description varchar(50) not null, --name that bill
@@ -13,8 +26,8 @@ create table paychecker.bills (
     track boolean not null default true --track payment
 )
 
-CREATE SCHEMA timetracker;
 
+CREATE SCHEMA timetracker;
 create table timetracker.timesheet (
     id serial primary key
     dt_entry timestamp not null
