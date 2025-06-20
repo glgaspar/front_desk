871dbcabb8c7f1bc2e960b87b684598878b87bff
Gustavo Gaspar <glgaspar@live.com>
2025-06-20 18:12:50 -0300

    creating taskmaster database (in progress)

--- connection/db.sql
+++ connection/db.sql
@@ -2,7 +2,7 @@ create database frontdesk;
 
 CREATE SCHEMA adm;
 CREATE TABLE adm.users (
-	id serial4 NOT NULL,
+	id serial NOT NULL,
 	username varchar(50) NOT NULL,
 	"password" varchar(255) NOT NULL,
 	CONSTRAINT users_pkey PRIMARY KEY (id)
@@ -32,3 +32,59 @@ create table timetracker.timesheet (
     id serial primary key
     dt_entry timestamp not null
 )
+
+create schema taskmaster;
+create table if not exists taskmaster.status (
+    id serial primary key,
+    name varchar(50) not null,
+    color varchar(7) not null
+);
+
+create table if not exists taskmaster.workspace (
+    id serial primary key,
+    name varchar(50) not null,
+    description text,
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP NULL
+);
+
+create table if not exists taskmaster.task (
+    id serial primary key,
+    title varchar(55) not null,
+    idUserCreated int not null FOREIGN KEY REFERENCES adm.users(id),
+    description text,
+    assignee int not null FOREIGN KEY REFERENCES adm.users(id), 
+    idStatus int not null FOREIGN KEY REFERENCES taskmaster.status(id),
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP NULL,
+    deadline   TIMESTAMP NULL,
+    idWorkspace int not null FOREIGN KEY REFERENCES taskmaster.workspace(id) ON DELETE CASCADE,
+);
+
+create table if not exists taskmaster.tag (
+    id serial primary key,
+    name varchar(50) not null,
+    color varchar(7) not null
+);
+
+create table if not exists taskmaster.comment (
+    id serial primary key,
+    idTask int not null FOREIGN KEY REFERENCES taskmaster.tasks(id) ON DELETE CASCADE,
+    idUser int not null FOREIGN KEY REFERENCES adm.users(id),
+    content text not null,
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+create table if not exists taskmaster.subtask (
+    idTask int not null FOREIGN KEY REFERENCES taskmaster.task(id) ON DELETE CASCADE,
+    id serial primary key,
+    title varchar(55) not null,
+    idUserCreated int not null FOREIGN KEY REFERENCES adm.users(id),
+    description text,
+    assignee int not null FOREIGN KEY REFERENCES adm.users(id), 
+    idStatus int not null FOREIGN KEY REFERENCES taskmaster.status(id),
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP NULL,
+    deadline   TIMESTAMP NULL,
+    idWorkspace int not null FOREIGN KEY REFERENCES taskmaster.workspace(id) ON DELETE CASCADE
+);
