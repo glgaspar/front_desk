82e5b390b9f2493056f6573e40e987ea5bacbcce
Gustavo Gaspar <glgaspar@live.com>
2025-09-24 18:36:11 -0300

    api handler

--- connection/connection.go
+++ connection/connection.go
@@ -1,9 +1,15 @@
 package connection
 
 import (
-	"fmt"
-	"os"
+	"bytes"
 	"database/sql"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
 	_ "github.com/lib/pq"
 )
 
@@ -25,4 +31,41 @@ func Db() (*sql.DB, error) {
 	err = conn.Ping()
 
 	return conn, err
+}
+
+func Api(method string, url string, headers map[string]string, data *any) (*[]byte, error) {
+	payload := &bytes.Buffer{}
+	if data != nil {
+		payloadBuffer, err := json.Marshal(data)
+		if err != nil {
+			return nil, err
+		}
+		payload = bytes.NewBuffer(payloadBuffer)
+	}
+	
+	req, err := http.NewRequest(method, url, payload)
+	if err != nil {
+		return nil, err
+	}
+
+	for key, value := range headers {
+		req.Header.Add(key,value)
+	}
+
+	response, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+	
+	if response.StatusCode < 200 || response.StatusCode > 226 { // just in case any of you go full semantic on returns. Accepting all 200s in http cats
+		return nil, errors.New(fmt.Sprintf("status: %d - ", response.StatusCode) + string(body))
+	}
+	
+	return &body, nil
 }
