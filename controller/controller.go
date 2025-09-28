dc3038cb64d40009dbdac155ecf1ec2ce27e1b54
Gustavo Gaspar <glgaspar@live.com>
2025-09-28 16:25:11 -0300

    no idea what im doing

--- controller/controller.go
+++ controller/controller.go
@@ -277,17 +277,17 @@ func GetLogs(c echo.Context) error {
 	c.Response().Header().Set("Content-Type", "text/event-stream")
 	c.Response().Header().Set("Cache-Control", "no-cache")
 	c.Response().Header().Set("Connection", "keep-alive")
-	go func(c *echo.Context) {
+	go func(c *echo.Context, logs *chan string) {
 		for {
 			select {
-			case line := <-logs:
+			case line := <-*logs:
 				(*c).Response().Write([]byte(line))
 				(*c).Response().Flush()
 			case <-(*c).Request().Context().Done():
 				return
 			}
 		}
-	}(&c)
+	}(&c, &logs)
 
 	err = app.GetLogs(&logs)
 	if err != nil {
